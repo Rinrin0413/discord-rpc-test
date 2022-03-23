328c0361b160d414ca1687f177cc0b77089bdf2c
Rinrin.rs <rinrin0413.valley@gmail.com>
2022-03-24 06:25:45 +0900

    INITIAL

---
+++ README.md
@@ -0,0 +1,5 @@
+# discord-rpc-test
+
+    discord-rpc-client で游ぶ
+
+[![MIT](https://img.shields.io/github/license/Rinrin0413/discord-rpc-test?color=%23A11D32&style=for-the-badge)](./LICENSE)
---
+++ src/main.rs
@@ -0,0 +1,37 @@
+extern crate discord_rpc_client;
+
+use std::{thread, time};
+use chrono::Utc;
+use discord_rpc_client::Client;
+
+fn main() {
+    // Get our main status message etc...
+    let status = ["👻Booo!!", "ʕ͡͡ ° ͜ʖ ͡°  ʔ"];
+    let detail = "自宅警備";
+
+    // Create the client
+    let mut drpc = Client::new(425407036495495169);
+
+    // Start up the client connection, so that we can actually send and receive stuff
+    drpc.start();
+
+    // Set the activity
+    drpc.set_activity(|act| 
+        act.state(status[0])
+            .state(status[1])
+            .details(detail)
+            .assets(|a|
+                a.large_text("あぁ～水素のお～と～♪") // 動かない！
+                .small_text("财力を见ㄝつけゑ！") // 動かない！
+            )
+            .timestamps(|ts| ts.start(Utc::now().timestamp() as u64))
+            .secrets(|s|
+                s.game("核実験".to_string())  // 動かない！
+                    .spectate("贵樣")  // 動かない！
+                    .join("加盟")  // ボタンだけは出る
+            )
+    ).expect("Failed to set activity");
+
+    // Wait 10 seconds before exiting
+    thread::sleep(time::Duration::from_secs(60));
+}
